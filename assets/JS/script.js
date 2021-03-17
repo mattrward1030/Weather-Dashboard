b3160680a962b3fec5503bb0f52a64a1f441b7c5
Matthew Ward <mattrward1030@gmail.com>
2021-03-16 23:05:04 -0400

    starting fucntion when search button is clicked

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -9,22 +9,32 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+var apiKey = "970bf4867656ea18a3d31d39295c5b09";
+var search = $(".searchButton");
 
 
+var keyCount = 0;
+search.click(function () {
 
-// function getWeather() {
-//     fetch(weatherData)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             console.log(data);
-//             for (var i = 0; i < data.length; i++) {
-//                 var currentTemp = document.createElement("p");
-//                 currentTemp.textContent = data[i].current;
-//                 weatherContainer.append(currentTemp);
-//             }
-//         });
-// }
+    var userInput = $(".userInput").val();
 
-// getWeather()
+    var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&Appid=" + apiKey + "&units=imperial";
+
+    var fiveDayWeather = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&Appid=" + apiKey + "&units=imperial";
+
+    if (userInput === "") {
+        alert("PLEASE ENTER A CITY")
+    }
+    else {
+        fetch(currentWeather)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                var cityName = $(".list-group").addClass("list-group-item");
+                cityName.append("<li>" + data.name + "</li>");
+                var local = localStorage.setItem(keyCount, data.name);
+                keyCount = keyCount + 1;
+            })
+    }
+})
