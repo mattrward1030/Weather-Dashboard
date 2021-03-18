3962691e0acc4dfbdcb96f8ebf04b5c865abc5c0
Matthew Ward <mattrward1030@gmail.com>
2021-03-18 09:39:28 -0400

    added comments

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -9,23 +9,29 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+
+
+
+// global variables
 var apiKey = "970bf4867656ea18a3d31d39295c5b09";
 var search = $(".searchButton");
 var cityBtn = $(".cityButton")
 var cityArray = []
 
+// on click function targeting the class .searchButton
 search.click(function () {
-
+    // getting city from user input field
     var userInput = $(".userInput").val();
-
+    // current weather api declared in a variable
     var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&Appid=" + apiKey + "&units=imperial";
-
+    // five day weather api delcared in a variable
     var fiveDayWeather = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&Appid=" + apiKey + "&units=imperial";
-
+    // if user does not input a city
     if (userInput === "") {
         alert("PLEASE ENTER A CITY")
     }
     else {
+        // fetching current weather data so certain criteria can be extracted
         fetch(currentWeather)
             .then(function (response) {
                 return response.json();
@@ -50,16 +56,16 @@ search.click(function () {
                 cityName.append(`<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png">`);
 
                 var cityTemp = cityName.append("<p>");
-
+                // appending city name card with all the current weather data
                 cityName.append(cityTemp);
                 cityTemp.append("<p>" + "Temperature: " + data.main.temp + " °F" + "</p>");
                 cityTemp.append("<p>" + "Humidity: " + data.main.humidity + " %" + "</p>");
                 cityTemp.append("<p>" + "Wind Speed: " + data.wind.speed + " mph" + "</p>");
 
-
+                // url for UV index
                 var uvUrl = `https://api.openweathermap.org/data/2.5/uvi?appid=b8ecb570e32c2e5042581abd004b71bb&lat=${data.coord.lat}&lon=${data.coord.lon}`;
 
-
+                // fetching UV index and selecting data.value out of it to get the UV
                 fetch(uvUrl)
                     .then(function (response) {
                         return response.json();
@@ -67,7 +73,7 @@ search.click(function () {
                     .then(function (data) {
                         var uvIndex = cityTemp.append("<p5>" + "UV Index: " + data.value + "</p5>").addClass("card-text")
                         if (uvIndex < 3) {
-                            p5.addClass("acceptable")
+                            $("p5").addClass("acceptable")
                         }
 
 
@@ -85,10 +91,11 @@ search.click(function () {
                 var fiveDayForecast = $(".fiveDayCard").addClass("card-body");
                 var fiveDayContainer = $(".fiveDay").addClass("card-text");
                 fiveDayContainer.empty();
+                // looping over the midDay array
                 midDay.forEach(function (i) {
                     var FiveDayTime = new Date(data.list[i].dt * 1000);
                     FiveDayTime = FiveDayTime.toLocaleDateString("en-US");
-
+                    // appending five day weather container to hold all five cards with the weather info
                     fiveDayContainer.append("<div class=blockColor>" + "<p>" + FiveDayTime + "</p>" + `<img src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png">` + "<p>" + "Temperature: " + data.list[i].main.temp + "</p>" + "<p>" + "Humidity: " + data.list[i].main.humidity + "%" + "</p>" + "</div>");
                 })
             })
@@ -98,7 +105,7 @@ search.click(function () {
 
 
 
-
+// trying to get an on click function for the search history buttons to work 
 cityBtn.click(function () {
 
     localStorage.getItem(cityArray)
